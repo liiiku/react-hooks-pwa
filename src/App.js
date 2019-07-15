fcafa2a9d81b051ab2a6cc879760a224bda628a8
ruinan.li02 <ruinan.li02@ele.me>
2019-07-15 15:23:15 +0800

    feat:react新特性context、contextType相关

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, createContext } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BatteryContext = createContext(90); // 这里面传递的默认值就是consumer找不到对应的provider的时候用到的
+
+class Leaf extends Component {
+  static contextType = BatteryContext;
+
+  render() {
+    const battery = this.context;
+    return (
+      <h1>Battery: {battery}</h1>
+    )
+  }
+}
+class Middle extends Component {
+  render() {
+    return <Leaf />
+  }
+}
+
+class App extends Component {
+  state = {
+    online: false,
+    battery: 60
+  }
+  render() {
+    const { battery, online } = this.state;
+
+    return (
+      <BatteryContext.Provider value={battery}>
+          <button type="button" onClick={() => this.setState({battery: battery - 1})}>减一</button>
+          <button type="button" onClick={() => this.setState({online: !online})}>切换</button>
+          <Middle />
+      </BatteryContext.Provider>
+    );
+  }
 }
 
 export default App;
