2e14d29c47d6b3b119e192bf01b9e1b77809982a
ruinan.li02 <ruinan.li02@ele.me>
2019-07-15 17:10:49 +0800

    feat:memo的简单使用

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { Component, PureComponent, memo } from 'react';
 import './App.css';
 
-const About = lazy(() => import(/* webpackChunkName: "about" */'./About'));
+// class Foo extends PureComponent {
+//   // 如果上面没有用pureComponent 的话， pureComponet 只有传入属性本身的对比，如果属性的内部发生了变化，就没有办法了
+//   // shouldComponentUpdate(nextProps, nextState) {
+//   //   console.log('nextProps', nextProps)
+//   //   if (nextProps.name === this.props.name) {
+//   //     return false;
+//   //   }
 
-// ErrorBoundary 捕获组件渲染错误的组件 componentDidCatch
+//   //   return true;
+//   // }
+//   render() {
+//     console.log('hello foo!')
+//     return null;
+//   }
+// }
+const Foo = memo(function Foo () {
+  console.log('foo render');
+  return (
+    <div>Foo 组件</div>
+  )
+})
 
 class App extends Component {
   state = {
-    hasError: false
+    count: 0
   }
-  // componentDidCatch() {
-  //   this.setState({
-  //     hasError: true
-  //   })
-  // }
-  static getDerivedStateFromError() {
-    return {
-      hasError: true
-    }
-  }
-  render() {
-    if (this.state.hasError) {
-      return <div>error!</div>
-    }
-
+  render () {
     return (
       <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <About />
-        </Suspense>
+        <button 
+          onClick={() => this.setState({count: this.state.count + 1})}
+        >加一</button>
+        <Foo name="App" />
       </div>
     )
   }
