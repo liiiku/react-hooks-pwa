61e21a4b0e4aedab0f92ed8a538a3649d8326a7d
ruinan.li02 <ruinan.li02@ele.me>
2019-07-16 17:46:17 +0800

    feat:useRef的简单用法

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React, { Component, useState, useMemo, memo, useCallback } from 'react';
+import React, { Component, PureComponent, useState, useMemo, memo, useEffect, useCallback, useRef } from 'react';
 
-const Counter = memo(function Counter(props) {
-  console.log('counter render');
-  return (
-    <h1 onClick={props.onClick}>{props.count}</h1>
-  )
-})
+// const Counter = memo(function Counter(props) {
+//   console.log('counter render');
+//   return (
+//     <h1 onClick={props.onClick}>{props.count}</h1>
+//   )
+// })
+
+class Counter extends PureComponent {
+  speak() {
+    console.log(`now counter is: ${this.props.count}`)
+  }
+  render() {
+    const { props } = this;
+    return (
+      <h1 onClick={props.onClick}>{props.count}</h1>
+    )
+  }
+}
 
 function App (props) {
   // 如果点击按钮的时候，setCount传入0，传入的的count的值没有变化，组件是不会重新渲染的
   const [count, setCount] = useState(0);
+  const counterRef = useRef();
+  let it = useRef();
+
+  useEffect(() => {
+    it.current = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (count >= 10) {
+      clearInterval(it.current);
+    }
+  })
 
   // 参数和useEffect是一样的
   // 调用时机是不一样的，useEffect 是副作用，所以在render 渲染之后再运行
@@ -36,7 +62,9 @@ function App (props) {
   // 如果useMemo返回的是一个函数，就可以用useCallback来省略顶层的函数
   const onClick = useCallback(() => {
       console.log('click');
-  }, []);
+      // console.log(counterRef.current);
+      counterRef.current.speak();
+  }, [counterRef]);
 
   return (
     <div>
@@ -46,7 +74,7 @@ function App (props) {
       >
         Click ({count}), Double: ({double}), Half: ({half})
       </button>
-      <Counter count={double} onClick={onClick} />
+      <Counter ref={counterRef} count={double} onClick={onClick} />
     </div>
   ) 
 }
